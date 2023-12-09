4620646bc652767ac07b579c70bca5cb51e41ea3
paspartu22 <andreyovodov@yandex.ru>
2023-12-09 13:31:09 +0300

    day09__

--- day09/main.py
+++ day09/main.py
@@ -1,20 +1,14 @@
 def find_next_number(valve):
-    old_array = valve
-    new_array = []
-    new_first_number = old_array[0]
-    new_last_number = old_array[-1]
-    line_num = 1
+    array = valve
+    result = [[array[0]],[array[-1]]]
     
-    while len(set(new_array)) != 1:   
-        new_array = [] 
-        for i in range(len(old_array)-1):
-            new_array.append(old_array[i+1] - old_array[i])
-        new_last_number += new_array[-1]
-        new_first_number += new_array[0] if line_num%2 == 0 else -new_array[0]
-        old_array = new_array
-        line_num += 1
-        
-    return new_first_number, new_last_number
+    while len(set(array)) != 1:   
+        array = [array[i+1] - array[i] for i in range(len(array)-1)]
+        result[0].append(array[0] if len(result[1])%2 == 0 else -array[0])
+        result[1].append(array[-1])
+    print(result)
+    print(f"{sum(result[0])} {sum(result[1])}")
+    return sum(result[0]), sum(result[1])
         
 def parse_input(file):
     valves = []
@@ -25,11 +19,14 @@ def parse_input(file):
     return valves
 
 def main():
-    valves = parse_input("data.txt")
+    valves = parse_input("test.txt")
     results = []
     for valve in valves:
         results.append(find_next_number(valve))
+    print("\nresult for part 1")
     print(sum([result[0] for result in results]))
+    print("result for part 2")
     print(sum([result[1] for result in results]))
+    
 if __name__ == "__main__":
     main()
